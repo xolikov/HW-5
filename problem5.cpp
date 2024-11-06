4eebc115d29e08a679c97448466de4fa2981b963
xolikov <a.xolikov@newuu.uz>
2024-11-06 17:34:35 +0500

    Create problem5.cpp

---
+++ problem5.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+    vector<char> small, big;
+    for (char c: s) {
+        if (c < 91) big.push_back(c);
+        else small.push_back(c);
+    }
+    cout << big.size();
+    for (char c: big) cout << ' ' << c;
+    cout << endl;
+    cout << small.size();
+    for (char c: small) cout << ' ' << c;
+    return 0;
+}
