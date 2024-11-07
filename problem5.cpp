e4278d2acf061ac59ebb83cea70e93f172e633e3
xolikov <a.xolikov@newuu.uz>
2024-11-07 09:27:13 +0500

    Update problem5.cpp

--- problem5.cpp
+++ problem5.cpp
@@ -8,10 +8,10 @@ int main() {
         if (c < 91) big.push_back(c);
         else small.push_back(c);
     }
-    cout << big.size();
-    for (char c: big) cout << ' ' << c;
+    cout << big.size() << ' ';
+    for (char c: big) cout << c << ' ';
     cout << endl;
-    cout << small.size();
-    for (char c: small) cout << ' ' << c;
+    cout << small.size() << ' ';
+    for (char c: small) cout << c << ' ';
     return 0;
 }
